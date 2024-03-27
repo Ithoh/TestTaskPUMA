e747e56436c1a752c69ea6a5e8dc1c7575cfc63f
Léo Delas <kay1900zz@gmail.com>
2024-03-27 16:08:53 +0100

    created db model

--- TestTaskPUMA/EmployeeTable/models.py
+++ TestTaskPUMA/EmployeeTable/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=200)
+    position = models.CharField(max_length=200)
+    office = models.CharField(max_length=200)
+    age = models.IntegerField(default=0)
+    start_date = models.DateTimeField("Start Date")
