f297f66782022c74a8aab453dbfc34f0e7ab5cc0
Léo Delas <kay1900zz@gmail.com>
2024-03-27 17:13:51 +0100

    Populated DB with data, changed type of start date in the model

--- TestTaskPUMA/EmployeeTable/models.py
+++ TestTaskPUMA/EmployeeTable/models.py
@@ -5,5 +5,9 @@ class Employee(models.Model):
     position = models.CharField(max_length=200)
     office = models.CharField(max_length=200)
     age = models.IntegerField(default=0)
-    start_date = models.DateTimeField("Start Date")
+    start_date = models.DateField("Start Date")
     salary = models.IntegerField(default=0)
+
+    # Self display name method
+    def __str__(self):
+        return self.name
