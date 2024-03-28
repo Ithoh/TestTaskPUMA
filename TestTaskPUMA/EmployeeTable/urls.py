46d0f24aa0919fe11dcd08e7408067e003fdb0e1
Léo Delas <kay1900zz@gmail.com>
2024-03-28 17:39:39 +0100

    Display table using RE:dom and Ag grid

--- TestTaskPUMA/EmployeeTable/urls.py
+++ TestTaskPUMA/EmployeeTable/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+# Needed if multiple views have similar names, to differentiate them
+app_name = 'EmployeeTable'
+
 urlpatterns = [
     path("", views.index, name="index"),
 ]
